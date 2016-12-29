b6def6bb10a0a8ffc9d59db9d9fd4877921cda80
unknown <anzarsh@mail.ru>
2016-12-29 17:19:46 +0300

    add index.php1

---
+++ includes/sledite_za_nami.php
@@ -0,0 +1,20 @@
+<section class="db-all-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="db-block-one">
+					<script type="text/javascript" src="//vk.com/js/api/openapi.js?136"></script>
+
+<!-- VK Widget -->
+<div id="vk_groups"></div>
+<script type="text/javascript">
+VK.Widgets.Group("vk_groups", {mode: 3, width: "340"}, 84915901);
+</script>
+				</div>
+			</div>
+			<div class="col-md-6">
+				
+			</div>
+		</div>
+	</div>
+</section>
