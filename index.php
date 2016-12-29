45caa300a16031d769fbe889c7fbee6bcf587031
engine77 <zaur.ka.00@mail.ru>
2016-12-29 17:06:44 +0300

    29

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 	<script src="js/common.js"></script>
 
 	<?php require_once('includes/header.php'); ?>
-	
+	<?php require_once('includes/napravlenia.php'); ?>
 	<?php require_once('includes/footer.php'); ?>
 
 	
