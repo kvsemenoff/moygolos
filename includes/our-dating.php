23fa391387b748b04f8ac1a437471f9b597423e7
engine77 <zaur.ka.00@mail.ru>
2016-12-30 17:01:48 +0300

    30

---
+++ includes/our-dating.php
@@ -0,0 +1,63 @@
+<div class="my-container">
+	<div class="df-dating">
+		<h3>Давайте знакомиться</h3>
+		<div class="df-left-box">
+			<div class="df-img2">
+				<!-- <img src="img/df-img-dating.jpg" alt="" class="img-responsive"> -->
+			</div>
+			<div class="df-img-little">
+				<div><a href="img/df-img-dating.jpg"><img src="img/df-img-dating1.jpg" alt=""></a></div>
+				<div><a href="img/df-img-dating2.jpg"><img src="img/df-img-dating2.jpg" alt=""></a></div>
+				<div><a href="img/df-img-dating3.jpg"><img src="img/df-img-dating3.jpg" alt=""></a></div>
+				<div><a href="img/df-img-dating4.jpg"><img src="img/df-img-dating4.jpg" alt=""></a></div>
+				<div><a href="img/df-img-dating5.jpg"><img src="img/df-img-dating5.jpg" alt=""></a></div>
+				<div><a href="img/df-img-dating6.jpg"><img src="img/df-img-dating6.jpg" alt=""></a></div>
+				<div><a href="img/df-img-dating7.jpg"><img src="img/df-img-dating7.jpg" alt=""></a></div>
+				<div><a href="img/df-img-dating2.jpg"><img src="img/df-img-dating7.jpg" alt=""></a></div>
+							
+			</div>
+		</div>
+		<div class="df-right-box">
+			<span class="df-span">Основатель студии «Мой голос»<span>Ксения Чадова</span></span>
+			<ul class="df-ulli">
+				<li>Педагог по вокалу и речи</li>
+				<li>VOICE-COACH</li>
+				<li>Вокалистка, создатель и исполнитель авторских песен, стихов</li>
+				<li>Дипломант, лауреат городских, областных, всероссийских и международных конкурсов в номинации «Вокал»</li>
+				<li>Участница городских концертных программ. Работала на главных сценах Екатеринбурга - Плотинка, сцена у Театра Эстрады, Площадь Обороны, парк Чкалова, парк Маяковского и другие</li>
+				<li>Средне-специальное музыкальное образование, специальность – Дирижерско-хоровое отделение (фортепьяно)</li>
+				<li>Выпускница кафедры телевидения и радиовещания факультета журналистики УрФУ</li>
+			</ul>
+			<a href="#" class="df-show-all">Показать больше</a>
+		</div>
+		<div class="df-left-float">
+			<h4>Ведущая курса «Вокал»</h4>
+			<span>Запись на курс уже идет</span>
+			<span><b>Февраль-Март</b> <em>(16 занятий, 24 часа)</em></span>
+			<span>Среда с 19:30 до 21:00, Суббота с 14:30 до 16:00</span>
+			<span>Стоимость курса: <b>7 000 руб.</b></span>
+			<span class="df-price">Запишитесь и получите  <span>скидку 500 руб.</span></span>
+			<a href="#">Записаться на курс</a>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+
+<script>
+$(document).ready(function() {
+	$('.df-img-little a').click(function(evt){
+		evt.preventDefault();
+		var imgPath = $(this).attr('href');
+		var oldImg = $('.df-img2 img');
+		var newImg = $('<img src=" '+ imgPath +' ">');
+		newImg.hide();
+		$('.df-img2').append(newImg);
+		newImg.fadeIn(300);
+
+		oldImg.slideDown(200, function(){
+			$(this).remove();
+		});
+	});
+	$('.df-img-little a:first').click();
+});
+</script>
