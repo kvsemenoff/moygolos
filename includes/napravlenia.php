5382e2ddc3d02725d4f1b1f2bc9c91e94c0810a8
engine77 <zaur.ka.00@mail.ru>
2016-12-30 10:31:16 +0300

    29

--- includes/napravlenia.php
+++ includes/napravlenia.php
@@ -4,9 +4,79 @@
 		<div class="df-img">
 			<div class="df-img-box">
 				<img src="img/df-img1.png" alt="">
-				<span>Вокал</span>
-
-
+				<div><span>Вокал</span></div>
+			</div>
+			<a href="#" class="df-link">Получить приглашение</a>
+			<div class="df-text">				
+			</div>
+		</div>
+		<div class="df-img">
+			<div class="df-img-box">
+				<img src="img/df-img2.png" alt="">
+				<div><span>Музыкальная
+				грамотность</span></div>
+			</div>
+			<a href="#" class="df-link">Получить приглашение</a>
+			<div class="df-text">				
+			</div>
+		</div>
+		<div class="df-img">
+			<div class="df-img-box">
+				<img src="img/df-img3.png" alt="">
+				<div><span>Техника речи и 
+				Ораторское 
+				искусство</span></div>
+			</div>
+			<a href="#" class="df-link">Получить приглашение</a>
+			<div class="df-text">				
+			</div>
+		</div>
+		<div class="df-img">
+			<div class="df-img-box">
+				<img src="img/df-img4.png" alt="">
+				<div><span>Актерская 
+				практика</span></div>
+			</div>
+			<a href="#" class="df-link">Получить приглашение</a>
+			<div class="df-text">				
+			</div>
+		</div>
+		<div class="df-img df-mr0">
+			<div class="df-img-box">
+				<img src="img/df-img5.png" alt="">
+				<div><span>Голосовая
+					практика 
+				с медитацией</span></div>
+			</div>
+			<a href="#" class="df-link">Получить приглашение</a>
+			<div class="df-text">				
+			</div>
+		</div>
+		<div class="df-img df-margin">
+			<div class="df-img-box">
+				<img src="img/df-img6.png" alt="">
+				<div><span>Психология
+				жизни</span></div>
+			</div>
+			<a href="#" class="df-link">Получить приглашение</a>
+			<div class="df-text">				
+			</div>
+		</div>
+		<div class="df-img df-margin">
+			<div class="df-img-box">
+				<img src="img/df-img7.png" alt="">
+				<div><span>Творчество
+				личности</span></div>
+			</div>
+			<a href="#" class="df-link">Получить приглашение</a>
+			<div class="df-text">				
+			</div>
+		</div>
+		<div class="df-img df-margin">
+			<div class="df-img-box">
+				<img src="img/df-img8.png" alt="">
+				<div><span>Свобода
+				движения</span></div>
 			</div>
 			<a href="#" class="df-link">Получить приглашение</a>
 			<div class="df-text">				
@@ -20,9 +90,9 @@
 <script>
 	$(function() {
 		$('.df-img').hover(function(){
-			$('.df-link').stop().fadeIn(700);
+			$('.df-link', this).stop().fadeIn(1100);
 		},function(){
-			$('.df-link').stop().fadeOut(0);
+			$('.df-link', this).stop().fadeOut(0);
 		});
 	});
 </script>
