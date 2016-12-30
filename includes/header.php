6b9a295edb83607a192afc0506d6d10f420268f9
Berzeg07 <berezg86@gmail.com>
2016-12-30 10:33:10 +0300

    29.14

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,37 @@
 <section class="dd-header-wrap">
 	<div class="dd-menu-wrap">
 		<div class="dd-menu-container">
-			<ul>
+			<ul class="dd-nav hidden-xs hidden-sm">
 				<li><a href="#">Главная</a></li>
 				<li><a href="#">Направления</a></li>
 				<li><a href="#">Преподаватели</a></li>
 				<li><a href="#">Фото с занятий</a></li>
 				<li><a href="#">Результаты</a></li>
-				<li><a href="#">Расписание и стоимость </a></li>
+				<li><a href="#">Расписание<br> и стоимость </a></li>
 				<li><a href="#">Отзывы</a></li>
 				<li><a href="#">Контакты</a></li>
 			</ul>
 		</div>
 	</div>
 </section>
+
+
+<div class="anz-menu hidden-lg hidden-md">
+	<input type="checkbox" id="check_1" class="anz-mobile"/>
+	<label class="anz-menu-ch anz-mobile" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i></label>
+	<div class="anz-perspective">
+		<div class="menu-list-pressed">
+			<ul class="ab-main-menu">
+				<li><a href="#">Главная</a></li>
+				<li><a href="#">Направления</a></li>
+				<li><a href="#">Преподаватели</a></li>
+				<li><a href="#">Фото с занятий</a></li>
+				<li><a href="#">Результаты</a></li>
+				<li><a href="#">Расписание<br> и стоимость </a></li>
+				<li><a href="#">Отзывы</a></li>
+				<li><a href="#">Контакты</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+</div>
