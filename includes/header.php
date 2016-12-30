7fccd9776b47a4737a4e83011cefcc3a202d5c98
Berzeg07 <berezg86@gmail.com>
2016-12-30 15:49:38 +0300

    29.14

--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,18 @@
 		</div>
 		<div class="anz-menu hidden-lg hidden-md">
 			<div class="dd-logo"><img src="img/logo.png" alt=""></div>
+			<div class="dd-logo-2">
+				<img src="img/logo2.png" alt="">
+			</div>
+			<div class="dd-txt-pl">
+				<span>Уроки вокала и речи в Екатеринбурге</span><br>
+				<span>ул. Декабристов 20, офис 413</span>
+			</div>
+			<div class="dd-fhone2">
+				<span>8 (912) 627-10-04</span><br>
+				<a href="#">Заказать звонок</a>
+			</div>
+			<div class="clearfix"></div>
 			<div class="dd-fhone"><span>8 (912) 627-10-04</span></div>
 			<input type="checkbox" id="check_1" class=""/>
 			<label class="anz-menu-ch " for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><img src="img/anz.png"></label>
@@ -34,31 +46,53 @@
 			</div>
 		</div>
 	</div>
-</section>
+	<div class="dd-header-content">
+		<div class="dd-title">
+			<span>Раскрой свой голос за <span>1 месяц</span></span>
+		</div>
+		<div class="dd-slogan">
+			<span> С помощью авторской методики преподавания
+				от вокалистки с 15-летним опытом в музыке</span>
+			</div>
+			<div class="dd-spisok">
+				<ul>
+				<li><span>Обучение в центре города</span></li>
+				<li><span>Различные направления</span></li>
+					<li><span>Групповые и индивидуальные
+						занятия</span></li>
+						<li><span>Дружеская атмосфера</span></li>
+						<li><span>Возможность выступить на
+							концерте</span></li>
+						</ul>
+						<a href="#">Получить приглашение</a>
+					</div>
+				</div>
+
+			</section>
 
 
-<script>
-	function navigation_scroll(){
-		var offset = $('.anz-menu').height() || $(window).height() || 150;
-		var scroll = $(document).scrollTop();
-		if (scroll < 20) {
-			$('.anz-menu')
-			.toggleClass('header-no-fixed', false)
-			.toggleClass('header-fixed', false);
-		}     
-		else if (scroll >= offset) {
-			$('.anz-menu')
-			.toggleClass('header-no-fixed', false)
-			.toggleClass('header-fixed', true);
+			<script>
+				function navigation_scroll(){
+					var offset = $('.anz-menu').height() || $(window).height() || 150;
+					var scroll = $(document).scrollTop();
+					if (scroll < 20) {
+						$('.anz-menu')
+						.toggleClass('header-no-fixed', false)
+						.toggleClass('header-fixed', false);
+					}     
+					else if (scroll >= offset) {
+						$('.anz-menu')
+						.toggleClass('header-no-fixed', false)
+						.toggleClass('header-fixed', true);
 
-		}
-		else if (scroll < offset - 50) {
-			$('.anz-menu')
-			.toggleClass('header-fixed', true)
-			.toggleClass('header-no-fixed', false);
-		}
-	}
-	$(document).scroll(function(){
-		navigation_scroll();
-	});
-</script>
+					}
+					else if (scroll < offset - 50) {
+						$('.anz-menu')
+						.toggleClass('header-fixed', true)
+						.toggleClass('header-no-fixed', false);
+					}
+				}
+				$(document).scroll(function(){
+					navigation_scroll();
+				});
+			</script>
