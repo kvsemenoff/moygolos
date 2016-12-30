e4cddffbbd4f7a89e2ac853039817960438d9a4a
Barasbi <betokov93@gmail.com>
2016-12-30 16:22:09 +0300

    table

--- includes/price.php
+++ includes/price.php
@@ -6,3 +6,88 @@
     </center>
 </div>
 
+<table 
+    id="dc-price_table" class="table table-condensed dc-price_table dc-white_color"
+    background="img/dc-table-background.jpg"
+>
+    <thead>
+	    <tr>
+	        <th class="dc-table_header2"></th>
+	        <th class="dc-table_header2">Стоимость</th>
+	        <th class="dc-table_header2">Кол-во занятий</th>
+	        <th class="dc-table_header2">Всего часов</th>
+	    </tr>
+    </thead>
+    <tbody>
+	    <tr class="dc-table_text">
+	        <td>
+	        	«Вокал»
+	        </td>
+	        <td>
+	            <b>7&nbsp000</b> руб.
+	        </td>
+	        <td>
+	            <b>16</b> занятий
+	        </td>
+	        <td>
+	            <b>24</b> часа
+	        </td>
+	    </tr>
+	    <tr class="dc-table_text">
+	        <td>
+	        	«Музыкальная грамотность»
+	        </td>
+	        <td>
+	            <b>7&nbsp000</b> руб.
+	        </td>
+	        <td>
+	            <b>16</b> занятий
+	        </td>
+	        <td>
+	            <b>24</b> часа
+	        </td>
+	    </tr>
+	    <tr class="dc-table_text">
+	        <td>
+	        	«Техника речи и Ораторское искусство»
+	        </td>
+	        <td>
+	            <b>9&nbsp000</b> руб.
+	        </td>
+	        <td>
+	            <b>16</b> занятий
+	        </td>
+	        <td>
+	            <b>24</b> часа
+	        </td>
+	    </tr>
+	    <tr class="dc-table_text">
+	        <td>
+	        	«Актерская практика»
+	        </td>
+	        <td>
+	            <b>5&nbsp500</b> руб.
+	        </td>
+	        <td>
+	            <b>8</b> занятий
+	        </td>
+	        <td>
+	            <b>16</b> часов
+	        </td>
+	    </tr>	    
+	    <tr class="dc-table_text">
+	        <td>
+	        	«Творчество личности»
+	        </td>
+	        <td>
+	            <b>5&nbsp500</b> руб.
+	        </td>
+	        <td>
+	            <b>8</b> занятий
+	        </td>
+	        <td>
+	            <b>16</b> часов
+	        </td>
+	    </tr>	    
+    </tbody>
+</table>
