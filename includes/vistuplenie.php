f6633017628a1769e0a7d482f5e5670f1cbb08a7
unknown <anzarsh@mail.ru>
2016-12-30 14:49:15 +0300

    add index.php1

---
+++ includes/vistuplenie.php
@@ -0,0 +1,16 @@
+<section class="db-block-sledite">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="db-block-sledite-one">
+					Выступление на международном фестивале МиксАрт-2015  авторская песня "Будь моей звездой"
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="db-block-sledite-two">
+				<iframe class="video"  src="https://www.youtube.com/embed/n2as1d6JzhI" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
