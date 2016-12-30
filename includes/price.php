e936fc0cbe31ecc830f0c32c3bd20ca2a8ff2eb7
Barasbi <betokov93@gmail.com>
2016-12-30 17:10:46 +0300

    table

--- includes/price.php
+++ includes/price.php
@@ -11,10 +11,7 @@
 <div id="dc-price-tables" class="dc-white_color">
     <center><h4>Стоимость курсов</h4></center>
 
-	<table 
-	    id="dc-price_table" class="table table-condensed dc-price_table dc-white_color"
-	    
-	>
+	<table id="dc-price_table" class="table table-condensed dc-price_table dc-white_color">
 	    <thead>
 		    <tr>
 		        <th class="dc-table_header2"></th>
@@ -24,7 +21,7 @@
 		    </tr>
 	    </thead>
 	    <tbody>
-		    <tr class="dc-table_text" style="border-bottom: 1px solid red;">
+		    <tr class="dc-table_text">
 		        <td>
 		        	«Вокал»
 		        </td>
@@ -96,4 +93,88 @@
 		    </tr>	    
 	    </tbody>
 	</table>
+
+	<center><h4>Стоимость групповых занятий</h4></center>
+
+	<table id="dc-price_table_group" class="table table-condensed dc-price_table dc-white_color">
+	    <thead>
+		    <tr>
+		        <th class="dc-table_header2"></th>
+		        <th class="dc-table_header2"><b>1</b> занятие</th>
+		        <th class="dc-table_header2"><b>4</b> занятия</th>
+		        <th class="dc-table_header2"><b>8</b> занятий</th>
+		    </tr>
+	    </thead>
+	    <tbody>
+		    <tr class="dc-table_text">
+		        <td>
+		        	Групповые занятия по направлению <br> «Голосовая практика»
+		        </td>
+		        <td>
+		            <b>400</b> руб.<br>
+		            1.5 часа
+		        </td>
+		        <td>
+		            <b>1&nbsp500</b> руб.<br>
+		            6 часов
+		        </td>
+		        <td>
+		            <b>3&nbsp000</b> руб.<br>
+		            12 часов
+		        </td>
+		    </tr>
+		    <tr class="dc-table_text">
+		        <td>
+		        	Групповые занятия по направлению <br> «Свобода движения»
+		        </td>
+		        <td>
+		            <b>400</b> руб.<br>
+		            1.5 часа
+		        </td>
+		        <td>
+		            <b>1&nbsp500</b> руб.<br>
+		            6 часов
+		        </td>
+		        <td>
+		            <b>3&nbsp000</b> руб.<br>
+		            12 часов
+		        </td>
+		    </tr>
+	    </tbody>
+	</table>
+
+
+	<center><h4>Стоимость индивидуальных занятий</h4></center>
+
+	<table id="dc-price_table_individual" class="table table-condensed dc-price_table dc-white_color">
+	    <thead>
+		    <tr>
+		        <th class="dc-table_header2"></th>
+		        <th class="dc-table_header2"><b>1</b> занятие</th>
+		        <th class="dc-table_header2"><b>4</b> занятия</th>
+		        <th class="dc-table_header2"><b>8</b> занятий</th>
+		    </tr>
+	    </thead>
+	    <tbody>
+		    <tr class="dc-table_text">
+		        <td>
+		        	Индивидуальные занятия по вокалу
+		        </td>
+		        <td>
+		            <b>1&nbsp000</b> руб.<br>
+		            1 час
+		        </td>
+		        <td>
+		            <b>3&nbsp600</b> руб.<br>
+		            4 часа
+		        </td>
+		        <td>
+		            <b>6&nbsp000</b> руб.<br>
+		            8 часов
+		        </td>
+		    </tr>
+	    </tbody>
+	</table>
+
+
 </div>
