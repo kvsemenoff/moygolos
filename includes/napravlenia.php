1615f6f143fff297c5309db2d35e46beffb58f8c
engine77 <zaur.ka.00@mail.ru>
2016-12-30 09:10:16 +0300

    29

---
+++ includes/napravlenia.php
@@ -0,0 +1,28 @@
+<div class="my-container">
+	<div class="df-napravlenia">
+		<h3>наши направления</h3>
+		<div class="df-img">
+			<div class="df-img-box">
+				<img src="img/df-img1.png" alt="">
+				<span>Вокал</span>
+
+
+			</div>
+			<a href="#" class="df-link">Получить приглашение</a>
+			<div class="df-text">				
+			</div>
+		</div>
+		
+	
+		
+	</div>
+</div>		
+<script>
+	$(function() {
+		$('.df-img').hover(function(){
+			$('.df-link').stop().fadeIn(700);
+		},function(){
+			$('.df-link').stop().fadeOut(0);
+		});
+	});
+</script>
