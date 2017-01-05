a6a9d1c989a0594ac8c27a1ede005500d311e591
Barasbi <betokov93@gmail.com>
2017-01-05 11:32:23 +0300

    how

--- includes/how.php
+++ includes/how.php
@@ -5,3 +5,54 @@
         </h3>
     </center>
 </div>
+
+<center>
+	<div class="dc-main-photo">
+	   <img src="img/dc-how-photo1.jpg">
+	</div>
+</center>
+
+<center>
+	<div class="dc-container">
+	    <div class="dc-box">
+			<div>
+			    <img class="dc-photo" src="img/dc-how-photo2.jpg">
+			</div>
+
+			<div>
+			    <img class="dc-photo" src="img/dc-how-photo3.jpg">
+			</div>
+
+			<div>
+			    <img class="dc-photo" src="img/dc-how-photo4.jpg">
+			</div>
+
+			<div>
+			    <img class="dc-photo" src="img/dc-how-photo5.jpg">
+			</div>
+
+			<div>
+			    <img class="dc-photo" src="img/dc-how-photo6.jpg">
+			</div>
+
+			<div>
+			    <img class="dc-photo" src="img/dc-how-photo7.jpg">
+			</div>
+
+			<div>
+			    <img class="dc-photo" src="img/dc-how-photo8.jpg">
+			</div>
+
+		</div>
+	</div>
+</center>
+
+<script>
+    $(document).ready(function() {
+        $(".dc-photo").click(
+        	function() {
+                $('.dc-main-photo').html('<img src="' + $(this).attr('src') + '" width="1170" height="585" />');
+        	}
+        );
+    });
+</script>
