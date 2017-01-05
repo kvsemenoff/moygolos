ac4d075fbbd250a781a46d4eefa629a9fe67130a
unknown <anzarsh@mail.ru>
2017-01-05 17:13:05 +0300

    add

--- includes/footer.php
+++ includes/footer.php
@@ -1,20 +1,17 @@
+
 <section class="db-footer">
-			<div class="db-footer-container">
-					<div class="db-footer-img">
-					<img src="../img/db-footer-logo1.png" alt="" class="img-responsive">
-					</div>
-				
-				
-					<div class="db-footer-text">
-						Уроки вокала и речи в Екатеринбурге<br>
-						<span>ул. Декабристов 20, офис 413</span>
-					</div>
-			
-				
-				<div class="db-footer-phone">
-						<span>8 (912) 627-10-04</span><br>
-						<a href="#">Заказать звонок</a>
-					</div>
-				<div class="clearfix"></div>
-			</div>	
+	<div class="db-footer-container">
+		<div class="db-footer-img">
+			<img src="../img/db-footer-logo1.png" alt="" class="img-responsive">
+		</div>
+		<div class="db-footer-text">
+			Уроки вокала и речи в Екатеринбурге<br>
+			<span>ул. Декабристов 20, офис 413</span>
+		</div>
+		<div class="db-footer-phone">
+			<span>8 (912) 627-10-04</span><br>
+			<a href="#">Заказать звонок</a>
+		</div>
+		<div class="clearfix"></div>
+	</div>	
 </section>
