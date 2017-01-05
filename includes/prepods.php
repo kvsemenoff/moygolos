7f8fa2c479ad588c2ea1c82b189ac657be008988
Berzeg07 <berezg86@gmail.com>
2017-01-05 15:30:17 +0300

    29.14

---
+++ includes/prepods.php
@@ -0,0 +1,177 @@
+<section class="dd-prepods">
+	<div class="my-container2">
+		<div class="dd-prep-title">
+			<span>Наши преподаватели</span>
+		</div>
+		<div class="dd-prep-wrap">
+			<div class="dd-prep">
+				<img class="img-responsive" src="img/p1.jpg" alt="">
+				<div class="dd-prep-txt">
+					<ul>
+						<li><span>Закончил Свердловский Колледж искусств и культуры (руководитель художественного коллектива)</span></li>
+						<li><span>6 лет проработал в музыкально-драматическом театре юного зрителя г.Нягянь, артист драмы</span></li>
+						<li><span>В его багаже более 15 спектаклей</span></li>
+						<li><span>Член Союза театральных деятелей</span></li>
+					</ul>
+					<div class="dd-prep-button-wrap">
+						<a class="dd-prep-button" href="#">Записаться на курс</a>
+					</div>
+					<div class="dd-button-span">
+						<span>Запись на курс уже идет<br>
+							<span>Февраль-Март</span> (8 занятий, 16 часов)<br>
+							Суббота с 12:00 до 14:00<br>
+							Стоимость курса: <span>5 500 руб.</span><br>
+							Запишитесь и получите скидку  <span class="dd-blue">500руб.</span></span>
+						</div>
+					</div>
+				</div>
+				<div class="dd-prep">
+					<img class="img-responsive" src="img/p2.jpg" alt="">
+					<div class="dd-prep-txt">
+						<ul>
+							<li><span>Редактор и ведущая программы «УТРОтв» на Областном телевидении </span></li>
+							<li><span>Ведущая утреннего шоу на «Радио без имени»</span></li>
+							<li><span>Выпускница курса «Ораторское мастерство» ЕГТИ</span></li>
+							<li><span>Выпускница факультета «Телерадиожурналистики» ГУ г.Екатеринбург </span></li>
+						</ul>
+						<div class="dd-prep-button-wrap">
+							<a class="dd-prep-button" href="#">Записаться на курс</a>
+						</div>
+						<div class="dd-button-span">
+							<span>Запись на курс уже идет<br>
+								<span>Февраль-Март</span> (8 занятий, 16 часов)<br>
+								Суббота с 12:00 до 14:00<br>
+								Стоимость курса: <span>5 500 руб.</span><br>
+								Запишитесь и получите скидку  <span class="dd-blue">500руб.</span></span>
+							</div>
+						</div>
+					</div>
+
+					<div class="dd-prep">
+						<img class="img-responsive" src="img/p3.jpg" alt="">
+						<div class="dd-prep-txt">
+							<ul>
+								<li><span>Звукорежиссер и музыкальный журналист</span></li>
+								<li><span>Училась в Уральской консерватории им. М.П.Мусоргского (кафедра звукорежиссуры)</span></li>
+								<li><span>Работает ведущей музыкального эфира на телеканале ЕТВ</span></li>
+								<li><span>Разбирается не только в теории музыки, но и в практике современной и классической музыки</span></li>
+							</ul>
+							<div class="dd-prep-button-wrap">
+								<a class="dd-prep-button" href="#">Записаться на курс</a>
+							</div>
+							<div class="dd-button-span">
+								<span>Запись на курс уже идет<br>
+									<span>Февраль-Март</span> (8 занятий, 16 часов)<br>
+									Суббота с 12:00 до 14:00<br>
+									Стоимость курса: <span>5 500 руб.</span><br>
+									Запишитесь и получите скидку  <span class="dd-blue">500руб.</span></span>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="dd-prep-wrap dd-prep-wrap2">
+						<div class="dd-prep">
+							<img class="img-responsive" src="img/p4.jpg" alt="">
+							<div class="dd-prep-txt">
+								<ul>
+									<li><span>Психолог-консультант, нейропсихолог</span></li>
+									<li><span>Квалифицированный теоретик, практикующий специалист</span></li>
+									<li><span>Выпускница УрГПУ, Институт психологии, специальность «Психология» (специалитет) </span></li>
+									<li><span>Выпускница программы "Клиническая психология в здравоохранении" Института естественных наук</span></li>
+								</ul>
+								<div class="dd-prep-button-wrap">
+									<a class="dd-prep-button" href="#">Записаться на курс</a>
+								</div>
+							</div>
+						</div>	
+						<div class="dd-prep dd-prep2">
+							<img class="img-responsive" src="img/p5.jpg" alt="">
+							<div class="dd-prep-txt">
+								<ul>
+									<li><span>Выпускница 1 ступени гештальт-терапии МИГиП</span></li>
+									<li><span>Ведущая танцевально-двигательных практик</span></li>
+									<li><span>Выпускница Института интегративной психологии и психотерапии</span></li>
+									<li><span>Провела более 180 часов танцевально-двигательной практики </span></li>
+								</ul>
+								<div class="dd-prep-button-wrap">
+									<a class="dd-prep-button" href="#">Записаться на курс</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="my-container2">
+			<div class="dd-t">
+				<span>Ведущие курса «Творчество личности»</span>
+			</div>
+			<div class="dd-big-border">
+				<div class="dd-prep2-wrap">
+					<div class="dd-pepod2 big-w big-w2">
+						<img class="img-responsive"  src="img/p6.jpg" alt="">
+						<div class="dd-prep-txt">
+							<ul>
+								<li><span>Выпускница ЕГТИ по специализации «Артист драматического театра и кино», мастерская А.А.Блинова</span></li>
+								<li><span>Действующая артистка театра «Волхонка» и «Театра им. Саманты Смит»</span></li>
+								<li><span>Помощник педагога по балету в ЕГТИ</span></li>
+							</ul>
+						</div>
+					</div>
+					<div class="dd-pepod2 dd-pepod3">
+						<img class="img-responsive" src="img/p7.jpg" alt="">
+						<div class="dd-prep-txt dd-width dd-float">
+							<ul>
+								<li><span>Выпускник ЕГТИ, специальность «Артист театра и кино» с квалификацией «Телеведущий», мастерская Двормана В.И.</span></li>
+								<li><span>Руководитель и основатель «Playground Studio»
+								</span></li>
+								<li><span>Ведущий и организатор музыкальных, поэтических мероприятий и мастер-классов</span></li>
+							</ul>
+						</div>
+						<div class="dd-prep-txt dd-width dd-float">
+							<ul>
+								<li><span>Выпускник ЕГТИ по специализации «Режиссура театра», мастерская Блинова А.А.</span></li>
+								<li><span>Основатель и режиссёр арт-студии «Театр им. Саманты Смит»</span></li>
+								<li><span>Фронтмэн блюз-мим-бэнда «Magical Marginals»</span></li>
+								<li><span>Писатель</span></li>
+							</ul>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="dd-pepod2 dd-pepod3 big-w">
+						<img class="img-responsive"  src="img/p8.jpg" alt="">
+						<div class="dd-prep-txt">
+							<ul>
+								<li><span>Выпускник ЕГТИ, специальность «Артист театра и кино» с квалификацией «Телеведущий», мастерская Двормана В.И.</span></li>
+								<li><span>Актер арт-студии «Театр им. Саманты Смит»</span></li>
+								<li><span>Поэт</span></li>
+								<li><span>Участник танцевальных групп</span></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				
+			<section class="dd-prepods">
+				<div class="my-container2">
+					<div class="dd-prep-wrap min-width">
+						<div class="dd-prep">
+							<div class="dd-prep-txt border-nones">
+								<div class="dd-prep-button-wrap">
+									<a class="dd-prep-button" href="#">Записаться на курс</a>
+								</div>
+								<div class="dd-button-span dd-button-span2">
+									<span>Запись на курс уже идет<br>
+										<span>Февраль-Март</span> (8 занятий, 16 часов)<br>
+										Суббота с 12:00 до 14:00<br>
+										Стоимость курса: <span>5 500 руб.</span><br>
+										Запишитесь и получите скидку  <span class="dd-blue">500руб.</span></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
